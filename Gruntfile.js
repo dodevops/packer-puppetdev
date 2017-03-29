52305748ec231a24d7236f26050549f4796d523b
Dennis Ploeger <dennis.ploeger@kps.com>
2017-03-29 11:58:07 +0200

    r10k modules fix, puppet4 for Debian 8.5 and Ubuntu 14.04

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,24 @@ module.exports = function (grunt) {
                     runPacker: true
                 }
             },
+            ubuntupuppet4: {
+                options: {
+                    template: 'ubuntu.json',
+                    templatePath: 'base.ubuntu',
+                    patches: [
+                        'patches/common.patch.json'
+                    ],
+                    only: [
+                        'virtualbox-iso'
+                    ],
+                    varFiles: [
+                        'ubuntu1404.json',
+                        '../vars/common.json',
+                        '../vars/ubuntupuppet4.json'
+                    ],
+                    runPacker: true
+                }
+            },
             ubuntu1604: {
                 options: {
                     template: 'ubuntu.json',
@@ -85,16 +103,44 @@ module.exports = function (grunt) {
                     ],
                     runPacker: true
                 }
+            },
+            debianpuppet4: {
+                options: {
+                    template: 'debian.json',
+                    templatePath: 'base.debian',
+                    patches: [
+                        'patches/common.patch.json'
+                    ],
+                    only: [
+                        'virtualbox-iso'
+                    ],
+                    varFiles: [
+                        'debian85.json',
+                        '../vars/common.json',
+                        '../vars/debianpuppet4.json'
+                    ],
+                    runPacker: true
+                }
             }
         },
         packertest: {
             ubuntu: {
                 options: {
                     boxPath: 'base.ubuntu/box/virtualbox/' +
-                        commonInfo.base_vm_name +
-                        '-ubuntu-' +
-                        commonInfo.version +
-                        '.box'
+                    commonInfo.base_vm_name +
+                    '-ubuntu-' +
+                    commonInfo.version +
+                    '.box'
+                }
+            },
+            ubuntupuppet4: {
+                options: {
+                    boxPath: 'base.ubuntu/box/virtualbox/' +
+                    commonInfo.base_vm_name +
+                    '-ubuntupuppet4-' +
+                    commonInfo.version +
+                    '.box',
+                    testpath: 'testpuppet4'
                 }
             },
             ubuntu1604: {
@@ -123,6 +169,16 @@ module.exports = function (grunt) {
                     '-debian-' +
                     commonInfo.version +
                     '.box'
+                }
+            },
+            debianpuppet4: {
+                options: {
+                    boxPath: 'base.debian/box/virtualbox/' +
+                    commonInfo.base_vm_name +
+                    '-debianpuppet4-' +
+                    commonInfo.version +
+                    '.box',
+                    testpath: 'testpuppet4'
                 }
             }
         }
@@ -168,18 +224,32 @@ module.exports = function (grunt) {
 
     grunt.registerTask('ubuntu', ['packerbuild:ubuntu', 'packertest:ubuntu']);
     grunt.registerTask(
+        'ubuntupuppet4',
+        [
+            'packerbuild:ubuntupuppet4',
+            'packertest:ubuntupuppet4'
+        ]
+    );
+    grunt.registerTask(
         'ubuntu1604',
         [
             'packerbuild:ubuntu1604',
             'packertest:ubuntu1604'
         ]
     );
-    grunt.registerTask('debian', ['packerbuild:debian', 'packertest:debian']);
     grunt.registerTask(
         'ubuntu1604puppet4',
         [
             'packerbuild:ubuntu1604puppet4',
             'packertest:ubuntu1604puppet4'
+        ]
+    );
+    grunt.registerTask('debian', ['packerbuild:debian', 'packertest:debian']);
+    grunt.registerTask(
+        'debianpuppet4',
+        [
+            'packerbuild:debianpuppet4',
+            'packertest:debianpuppet4'
         ]
     );
 
